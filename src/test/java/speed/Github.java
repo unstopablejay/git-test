d1ac6dfc788d12271f68f3601d1ee7d394e16776
unstopablejay <unstopablejay@gmail.com>
2022-01-13 12:49:28 +0530

    test

---
+++ src/test/java/speed/Github.java
@@ -0,0 +1,13 @@
+package speed;
+
+public class Github {
+
+	public void git() {
+System.out.println("pharmacy@dhani.com");
+	}
+	
+	public static void main(String[] args) {
+		Github g = new Github();
+		g.git();
+	}
+}
